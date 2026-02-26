2ef93064acfc8539cdc25c464a4e2c54cc3a0b10
altokan <amjad.tokan@gmail.com>
2026-02-26 18:21:29 +0100

    Update firebase-messaging-sw.js

--- Public/firebase-messaging-sw.js
+++ Public/firebase-messaging-sw.js
@@ -4,12 +4,12 @@ importScripts("https://www.gstatic.com/firebasejs/10.12.2/firebase-app-compat.js
 importScripts("https://www.gstatic.com/firebasejs/10.12.2/firebase-messaging-compat.js");
 
 firebase.initializeApp({
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_STORAGE_BUCKET",
+  apiKey: "AIzaSyDKmuJk62Q-cfSBUF9fzfWskx7Kq5yChHc",
+  authDomain: "calender-new-app.firebaseapp.com",
+  projectId: "calender-new-app",
+  storageBucket: "calender-new-app.firebasestorage.app",
   messagingSenderId: "436086002651",
-  appId: "YOUR_APP_ID"
+  appId: "1:436086002651:web:42a2d5c406634037d40dfe"
 });
 
 const messaging = firebase.messaging();
